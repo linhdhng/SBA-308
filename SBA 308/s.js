61648da8e0485d302aa0493de24f65aa7ad275b3
linhdhng <153128184+linhdhng@users.noreply.github.com>
2024-01-27 17:52:07 -0600

    Update s.js

--- SBA 308/s.js
+++ SBA 308/s.js
@@ -1,14 +1,12 @@
-
 // If the learner’s submission is late (submitted_at is past due_at), 
 // deduct 10 percent of the total points possible from their score for that assignment.
-// The provided course information.
+
 const CourseInfo = {
     id: 451,
     name: "Introduction to JavaScript"
   };
-  
-  // The provided assignment group.
-  const AssignmentGroup = {
+
+const AssignmentGroup = {
     id: 12345,
     name: "Fundamentals of JavaScript",
     course_id: 451,
@@ -35,8 +33,7 @@ const CourseInfo = {
     ]
   };
   
-  // The provided learner submission data.
-  const LearnerSubmissions = [
+const LearnerSubmissions = [
     {
       learner_id: 125,
       assignment_id: 1,
@@ -78,25 +75,23 @@ const CourseInfo = {
       }
     }
   ];
+  
 function getLearnerData(courseInfo, assignmentGroup, learnerSubmissions) {
     //combine data from given arrays into one array
-    const currentDate = new Date();//declare a current date for comparision
+    const currentDate = new Date()//declare a current date for comparision
     const combinedData = LearnerSubmissions.flatMap(submission => {//use flatMap to iterate over each submission in LearnerSubmissions and map them to the combined data.
     const assignment = AssignmentGroup.assignments.find(assignment =>//use find method to search for assignment in AssignmentGroup.assignments based on the assignment_id.
         assignment.id === submission.assignment_id && new Date(assignment.due_at) <= currentDate)//check for the assignments that has passed the due date;
         if (assignment) { //if assignment is found, return object with combined data
             return {
-            id: submission.learner_id,
-            assignment_id: submission.assignment_id,
-            due_date: assignment.due_at,
-            points_possible: assignment.points_possible,
-            submitted_at: submission.submission.submitted_at,
-            score: submission.submission.score
-            }
-        } 
-        else return [] //if assignment is not found return empty array
+                id: submission.learner_id,
+                avg: parseFloat((submission.submission.score / assignment.points_possible).toFixed(2)),//round result to 2 decimals and change it into number using parseFloat
+                assignmentId: submission.assignment_id,
+              }
+            return [] //if assignment is not found return empty array
+        }
     })
     console.log(combinedData)
 }
 
-console.log(getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions));
+console.log(getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions))
